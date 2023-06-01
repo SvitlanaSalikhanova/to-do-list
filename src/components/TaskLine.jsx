31588945ab7275040b00cc04b9b31dcee7da0591
Svitlana Salikhanova <s.salikhanova@godeltech.com>
2023-06-01 13:17:26 +0200

    show delete button using css

--- src/components/TaskLine.jsx
+++ src/components/TaskLine.jsx
@@ -4,12 +4,9 @@ import DeleteButton from './DeleteButton';
 import TaskLineCss from '../styles/TaskLine.module.scss';
 
 export default function TaskLine({ item, style }) {
-    const [showDeleteButton, setShowDeleteButton] = React.useState(false);
     return (
         <div
             className={TaskLineCss.taskLine}
-            onMouseEnter={() => setShowDeleteButton(true)}
-            onMouseLeave={() => setShowDeleteButton(false)}
             style={style}
         >
             <Check item={item} />
@@ -23,8 +20,9 @@ export default function TaskLine({ item, style }) {
             >
                 {item.text}
             </div>
-
-            {showDeleteButton && <DeleteButton item={item} />}
+            <div className={TaskLineCss.deleteBlock}>
+                <DeleteButton item={item} />
+            </div>
         </div>
     );
 }
