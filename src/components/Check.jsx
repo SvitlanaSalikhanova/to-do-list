6cd138ac5bdd8c88880e0f3868ada2f222b1fde0
Svitlana Salikhanova <s.salikhanova@godeltech.com>
2023-06-01 11:23:43 +0200

    replaced button with checkbox

--- src/components/Check.jsx
+++ src/components/Check.jsx
@@ -1,18 +1,22 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { BsCheckCircle, BsCircle } from 'react-icons/bs';
 import { completeTask } from '../redux/TasksSlice';
 import CheckCss from '../styles/Check.module.scss';
 
 export default function Check({ item }) {
     const dispatch = useDispatch();
     return (
-        <button
-            type="button"
-            className={CheckCss.doneCheck}
-            onClick={() => dispatch(completeTask(item.id))}
-        >
-            {item.isDone ? <BsCheckCircle className="BsCheckCircle" /> : <BsCircle className="BsCircle" />}
-        </button>
+        <div className={CheckCss.container}>
+            <div className={CheckCss.round}>
+                <input
+                    type="checkbox"
+                    checked={item.isDone}
+                    onClick={() => dispatch(completeTask(item.id))}
+                    id={`checkbox${item.id}`}
+                />
+                <label htmlFor={`checkbox${item.id}`} />
+            </div>
+        </div>
     );
 }
